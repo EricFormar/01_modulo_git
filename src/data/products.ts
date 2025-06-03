fff6905dc289c4b448a4813d7b70e46793902d2a
Eric <menaericdaniel@gmail.com>
2025-06-02 21:14:28 -0300

    feat : add model Product & products array

---
+++ src/data/products.ts
@@ -0,0 +1,8 @@
+import { Product } from "../models/Product";
+
+const products: Product[] = [
+    { id: 1, nombre: "Laptop", precio: 999.99, descripcion: "Laptop gaming" },
+    { id: 2, nombre: "Mouse", precio: 29.99 }
+];
+
+export default products;

5c62edcf22c4785140da7419e6249235135af964
Eric <menaericdaniel@gmail.com>
2025-06-02 21:21:46 -0300

    feat: add routing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import productsRouter from './routes/product.routes'; 
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api', productsRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
